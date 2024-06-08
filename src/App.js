5f7cbf591a646ebc9add49216c8b909d16411427
C1ZC <camilo.zavala.c@gmail.com>
2024-06-08 15:59:42 -0400

    chore: Actualizar dependencias de npm a las versiones más recientes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import ListadoEmpleados from "./empleados/ListadoEmpleados";
+import Navegacion from "./plantilla/Navegacion";
+import AgregarEmpleado from "./empleados/AgregarEmpleado";
 
 function App() {
   return (
+          <div className="container">
+            <BrowserRouter>
+              <Navegacion/>
+              <Routes>
+                <Route exact path='/' element={<ListadoEmpleados/>} />
+                <Route exact path='/agregar' element={<AgregarEmpleado/>}/>
 
-        <ListadoEmpleados/>
+              </Routes>
+            </BrowserRouter>
+
+          </div>
+
 
   );
 }
